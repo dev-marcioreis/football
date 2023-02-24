482ffa99be95321778677a5d3717596d2ab642e0
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-24 16:39:40 -0300

    Iniciando header section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import Header from './common/header/Header';
-import Main from './components/main/Main'
-import Footer from './common/footer/Footer';
-
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
-      <Footer />
     </>
   );
 }
--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,13 +1,27 @@
 import './header.css'
+import Logo from '../../assets/logo.png'
+import { AiOutlineMenu as Menu, AiOutlineClose as Close, AiOutlineSearch as Search } from 'react-icons/ai'
 
 const Header = () => {
   return (
     <>
-      <section className='section header'>
+      <header className='header'>
           <div className="container">
-              <h1>Header</h1>
+              <div className="header__container flex">
+                <div className="header__toggle">
+                  <span><Menu /></span>
+                  <span><Close /></span>
+                </div>
+                <div className="header__logo flex">
+                  <img src={Logo} alt="Logo" />
+                  <h2>football</h2>
+                </div>
+                <div className="header__search">
+                  <span><Search /></span>
+                </div>
+              </div>
           </div>
-      </section>
+      </header>
     </>
   )
 }
