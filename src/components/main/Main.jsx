3a1b3bc5329a38e919b504640ea7ecb15c3b7f18
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-24 17:11:50 -0300

    Inicar teste API

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,35 @@
-import './style.css'
+import './main.css'
+import LeagueLogo from '../../assets/league.png'
+import TeamLogo1 from '../../assets/team1.png'
+import TeamLogo2 from '../../assets/team2.png'
 
 const Main = () => {
   return (
     <>
         <section className="section main">
-            <div className="container">
-                <h1>Main</h1>
+          <button>
+            Test API
+          </button>
+            <div className="container live-score flex">
+                <div className="live-container">
+                  <div className="match-container">
+                    <div className="league flex">
+                      <img src={LeagueLogo} alt="" className="league-logo" />
+                      <h4 className="league-name">campeonato brasileiro</h4>
+                    </div>
+                    <div className="teams-container flex">
+                      <div className="team-box">
+                        <img src={TeamLogo1} alt="" className="team-logo" />
+                        <h4 className="team-name">time da casa</h4>
+                      </div>
+                      <span className="result">1 x 1</span> 
+                      <div className="team-box">
+                        <img src={TeamLogo2} alt="" className="team-logo" />
+                        <h4 className="team-name">time visitante</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
             </div>
         </section>
     </>
