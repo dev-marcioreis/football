d4b1c79c90efe5da02d707fcc15c25295f3d9a62
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-28 09:36:48 -0300

    Mudança no projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './common/header/Header';
 import Main from './components/main/Main'
+import Footer from './common/footer/Footer'
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,27 +1,13 @@
 import './header.css'
-import Logo from '../../assets/logo.png'
-import { AiOutlineMenu as Menu, AiOutlineClose as Close, AiOutlineSearch as Search } from 'react-icons/ai'
 
 const Header = () => {
   return (
     <>
-      <header className='header'>
-          <div className="container">
-              <div className="header__container flex">
-                <div className="header__toggle">
-                  <span><Menu /></span>
-                  <span><Close /></span>
-                </div>
-                <div className="header__logo flex">
-                  <img src={Logo} alt="Logo" />
-                  <h2>football</h2>
-                </div>
-                <div className="header__search">
-                  <span><Search /></span>
-                </div>
-              </div>
-          </div>
-      </header>
+      <section className='section header'>
+            <div className='container'>
+                <h1>Header</h1>
+            </div>
+        </section>
     </>
   )
 }
